1660e4f396ed3a071e3e72fc5160c466950090d3
yugal-compro <yugal.vashishth@comprotechnologies.com>
2024-02-29 11:49:01 +0530

    first

---
+++ apps/app1/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+  import s from "supervillains";
+  import g from "good";
+  console.log(s);
+  console.log(g);
+</script>
+
